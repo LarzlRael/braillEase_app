b58d85d585cc22f415913b75348a6f52aafa0061
Reynaldo GT <larz.dev@gmail.com>
2023-07-10 18:32:01 -0400

    added animation to enter

--- lib/pages/phrase_maker_page.dart
+++ lib/pages/phrase_maker_page.dart
@@ -65,95 +65,105 @@ class _PhraseMakerPageState extends State<PhraseMakerPage> {
             ),
         ],
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: MediaQuery.of(context).size.height * .65,
-              child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 5,
-                  crossAxisSpacing: 1,
-                  mainAxisSpacing: 1,
-                  childAspectRatio: MediaQuery.of(context).size.width /
-                      (MediaQuery.of(context).size.height / 1.15),
-                ),
-                shrinkWrap: true,
-                itemCount: listGenerate.length,
-                itemBuilder: (_, int index) {
-                  final letter = phrase[index];
-                  final listGenerated = listGenerate[index];
-                  return GestureDetector(
-                    onTap: () {
-                      /* globalProvider.showSnackBar(
+      body: phrase.isEmpty
+          ? NoInformation(
+              icon: Icons.info_outline,
+              text: "Pulsa en el boton + para agregar letras",
+              showButton: false,
+              iconButton: Icons.add_a_photo,
+            )
+          : SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    height: MediaQuery.of(context).size.height * .65,
+                    child: GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 5,
+                        crossAxisSpacing: 1,
+                        mainAxisSpacing: 1,
+                        childAspectRatio: MediaQuery.of(context).size.width /
+                            (MediaQuery.of(context).size.height / 1.15),
+                      ),
+                      shrinkWrap: true,
+                      itemCount: listGenerate.length,
+                      itemBuilder: (_, int index) {
+                        final letter = phrase[index];
+                        final listGenerated = listGenerate[index];
+                        return GestureDetector(
+                          onTap: () {
+                            /* globalProvider.showSnackBar(
                         context,
                         letter,
                         backgroundColor: Colors.blue,
                       ); */
-                    },
-                    child: Stack(
-                      children: [
-                        BraileLetterCardPickeed(
-                          globalProvider: globalProvider,
-                          listGenerated: listGenerated,
-                          letter: letter,
-                        ),
-                        Positioned(
-                          top: -13,
-                          right: -13,
-                          child: IconButton(
-                            onPressed: () {
-                              setState(() {
-                                phrase = phrase.replaceFirst(letter, '');
-                              });
-                            },
-                            icon: Icon(
-                              Icons.cancel,
-                              size: 15,
-                              color: Colors.white,
-                            ),
+                          },
+                          child: Stack(
+                            children: [
+                              FadeInOpacity(
+                                duration: Duration(milliseconds: 750),
+                                child: BraileLetterCardPickeed(
+                                  globalProvider: globalProvider,
+                                  listGenerated: listGenerated,
+                                  letter: letter,
+                                ),
+                              ),
+                              Positioned(
+                                top: -13,
+                                right: -13,
+                                child: IconButton(
+                                  onPressed: () {
+                                    setState(() {
+                                      phrase = phrase.replaceFirst(letter, '');
+                                    });
+                                  },
+                                  icon: Icon(
+                                    Icons.cancel,
+                                    size: 15,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
-                        ),
-                      ],
+                        );
+                      },
                     ),
-                  );
-                },
-              ),
-            ),
-            ListTile(
-              title: Container(
-                width: double.infinity,
-                child: Text(
-                  phrase,
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
                   ),
-                ),
-                padding: EdgeInsets.all(10),
-              ),
-              trailing: IconButton(
-                onPressed: () async {
-                  await Clipboard.setData(ClipboardData(text: phrase));
+                  ListTile(
+                    title: Container(
+                      width: double.infinity,
+                      child: Text(
+                        phrase,
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10),
+                    ),
+                    trailing: IconButton(
+                      onPressed: () async {
+                        await Clipboard.setData(ClipboardData(text: phrase));
 
-                  globalProvider.showSnackBar(
-                    backgroundColor: Colors.blue,
-                    context,
-                    "Texto copiado al portapapeles",
-                  );
-                },
-                icon: phrase.isNotEmpty
-                    ? Icon(
-                        Icons.copy,
-                        color: globalProvider.pickerColor,
-                      )
-                    : SizedBox(),
+                        globalProvider.showSnackBar(
+                          backgroundColor: Colors.blue,
+                          context,
+                          "Texto copiado al portapapeles",
+                        );
+                      },
+                      icon: phrase.isNotEmpty
+                          ? Icon(
+                              Icons.copy,
+                              color: globalProvider.pickerColor,
+                            )
+                          : SizedBox(),
+                    ),
+                  ),
+                ],
               ),
             ),
-          ],
-        ),
-      ),
     );
   }
 
--- lib/widgets/anim/fade_in_opacity.dart
+++ lib/widgets/anim/fade_in_opacity.dart
@@ -2,14 +2,15 @@ part of '../widgets.dart';
 
 class FadeInOpacity extends StatelessWidget {
   final Widget child;
+  final Duration? duration;
 
-  const FadeInOpacity({super.key, required this.child});
+  const FadeInOpacity({super.key, required this.child, this.duration});
   @override
   Widget build(BuildContext context) {
     return TweenAnimationBuilder(
       tween: Tween<double>(begin: 0, end: 1),
       curve: Curves.easeIn,
-      duration: Duration(milliseconds: 750),
+      duration: duration ?? Duration(milliseconds: 750),
       child: child,
       builder: (BuildContext context, double value, Widget? child) {
         return Opacity(
